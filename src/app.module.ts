fd6cb57e2787111276b2f7c8d0cb0b10535eb836
Eric Mulvihill <eric.mulvihill@gmail.com>
2024-12-08 22:30:12 -0500

    refactor: update configuration files and improve test setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,24 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { join } from 'path';
 import { VideoSnapModule } from './video-snap/video-snap.module';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver,
-            typePaths: ['./**/*.graphql'],
-            definitions: {
-                path: join(process.cwd(), 'src/graphql.ts'),
-            },
-        }),
-        VideoSnapModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+    VideoSnapModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
 }
